97b8a7cc4bc8ab5b1615ed86768e8156a6d58d4d
Yeeun Lee <77772720+YeeunLee8245@users.noreply.github.com>
2021-07-30 17:33:39 +0900

    Create MainActivity.kt

---
+++ Thread&Service/Fragment/MainActivity.kt
@@ -0,0 +1,30 @@
+package kr.co.yeaeun.viewbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // fragment 객체 생성
+        val frag1 = FirstFragment()
+
+        button.setOnClickListener {
+            // Fragment 작업 시작
+             val tran = supportFragmentManager.beginTransaction()
+            // Fragment를 셋팅한다.
+            tran.add(R.id.container1,frag1) // 위치(레이아웃)을 지정해줘야함
+            //tran.replace(R.id.container1,frag1) // 대체한다.
+
+            // 백버튼 클릭시 Activity 종료X, 해당 Fragment 종료
+            tran.addToBackStack(null) // backstack에 삽입(1번 인자: stack 안에서 구별하기 위한 이름)
+            tran.commit()
+        }
+
+    }
+
+}
