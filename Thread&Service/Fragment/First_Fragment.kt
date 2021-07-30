90c1373861d99b3fecda9ec28f36a461fcabb6a8
Yeeun Lee <77772720+YeeunLee8245@users.noreply.github.com>
2021-07-30 17:34:05 +0900

    Create First_Fragment.kt

---
+++ Thread&Service/Fragment/First_Fragment.kt
@@ -0,0 +1,17 @@
+package kr.co.yeaeun.viewbasic
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FirstFragment: Fragment() {
+
+    // 여기에서 만든 View를 Fragment의 View로 사용한다.
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // 넘어온 LayoutInflater로 View 만들기
+        val view = inflater.inflate(R.layout.fragment_first, null)
+        return view
+    }
+}
