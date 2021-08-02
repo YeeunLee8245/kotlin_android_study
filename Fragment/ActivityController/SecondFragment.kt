5ba27e09c1293c41d3e6598b6de526f414307428
Yeeun Lee <77772720+YeeunLee8245@users.noreply.github.com>
2021-08-02 14:17:53 +0900

    Create SecondFragment.kt

---
+++ Fragment/ActivityController/SecondFragment.kt
@@ -0,0 +1,27 @@
+package kr.co.yeaeun.viewbasic
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment: Fragment() {
+
+    // (*중요) 프래그먼트를 통해 보여줄 View를 생성하기 위해 호출
+    // 여기에서 만든 View를 Fragment의 View로 사용한다.
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // 넘어온 LayoutInflater로 View 만들기
+        val view = inflater.inflate(R.layout.fragment_second, null)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mainActivity = activity as MainActivity
+        result_text1.text = mainActivity.value1
+        result_text2.text = mainActivity.value2
+    }
+}
